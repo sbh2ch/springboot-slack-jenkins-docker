f9a28c30b428d0670b804c56a9c677c47f58c013
kiost-PC\kiost <sbh2ch@gmail.com>
2017-07-09 18:54:52 +0900

    jenkins connect

---
+++ src/main/java/kr/kosc/Application.java
@@ -0,0 +1,14 @@
+package kr.kosc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by kiost on 2017-07-09.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
